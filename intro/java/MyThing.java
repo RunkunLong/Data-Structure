44751ee30a8e2c81e05e05e2134b82783de03c0b
runkun.long <runkun.long@ACCOUNTS.AD.WUSTL.EDU>
2015-08-30 00:12:10 +0000

    final text

--- intro/java/MyThing.java
+++ intro/java/MyThing.java
@@ -6,13 +6,14 @@ public class MyThing {
 	{System.exit(-1);}
 	else
 	{
+		
 		int j=0;
-		for(int k=1;k+1<x;k=k+2)
+		for(int k=1;k<x & k+1!=x;k=k+2,j=k)
 			{
-		    System.out.println(k+"...");
-		    j=k;
+		    System.out.println(k+"...");  
 			}
-	  System.out.println(j+"!");
+	   System.out.println(j+"!");
+	  
 		}
     // Print every odd number between 1 and x (inclusive) in the form
 	// 1...
